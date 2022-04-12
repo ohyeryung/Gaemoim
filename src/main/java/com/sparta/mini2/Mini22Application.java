6114cd9b4306434829c3c9e53f33e5151d71ce0a
ohyeryung <100130070+ohyeryung@users.noreply.github.com>
2022-04-12 17:01:50 +0900

    mini22 application timezone 추가

--- src/main/java/com/sparta/mini2/Mini22Application.java
+++ src/main/java/com/sparta/mini2/Mini22Application.java
@@ -2,12 +2,20 @@ package com.sparta.mini2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class Mini22Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Mini22Application.class, args);
     }
-
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
