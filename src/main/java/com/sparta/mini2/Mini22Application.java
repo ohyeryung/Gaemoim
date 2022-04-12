5c3a76864a70f306b9a61ba19826ac12c919179b
ohyeryung <100130070+ohyeryung@users.noreply.github.com>
2022-04-12 15:25:46 +0900

    Initial commit

---
+++ src/main/java/com/sparta/mini2/Mini22Application.java
@@ -0,0 +1,13 @@
+package com.sparta.mini2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mini22Application.class, args);
+    }
+
+}
