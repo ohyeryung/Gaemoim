e3adfb998278d4f7d722e1d33a7eafa858a36d31
ohyeryung <100130070+ohyeryung@users.noreply.github.com>
2022-04-13 21:00:26 +0900

    파일 재생성

---
+++ src/main/java/com/sparta/mini2/controller/BackController.java
@@ -0,0 +1,26 @@
+package com.sparta.mini2.controller;
+
+import com.sparta.mini2.dto.BackResponseDto;
+import com.sparta.mini2.security.UserDetailsImpl;
+import com.sparta.mini2.service.BackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackController {
+    private final BackService backService;
+
+    @Autowired
+    public BackController(BackService backService) {
+        this.backService = backService;
+    }
+
+
+    @PostMapping("/api/back/{postId}")
+    public BackResponseDto clickBack(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return backService.clickBack(postId, userDetails);
+    }
+}
