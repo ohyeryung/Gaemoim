d44b72a4e927aaea77f267783797a60b37b4214a
Sounghun Um <korea970112@naver.com>
2022-04-13 23:32:24 +0900

    예외처리 정원완료 후, 참여자가 취소할 경우, 다시 정원완료 false 전환

--- src/main/java/com/sparta/mini2/service/BackService.java
+++ src/main/java/com/sparta/mini2/service/BackService.java
@@ -27,18 +27,18 @@ public class BackService {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
 
-        @Transactional
+    @Transactional
     public BackResponseDto clickBack(Long postId, UserDetailsImpl userDetails) {
         BackRequestDto backRequestDto = new BackRequestDto();
         // 해당 게시글을 전부 찾기
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("찾으시는 글이 존재하지 않습니다.")
         );
-            // 다른 포지션에서 참여를 햇는지 검사
-            Front frontCheck = frontRepository.findByUserAndPost(userDetails.getUser(), post).orElse(null);
-            if (frontCheck != null){
-                throw new IllegalArgumentException("이미 프론트엔드에 참여를 하셨습니다.");
-            }
+        // 다른 포지션에서 참여를 햇는지 검사
+        Front frontCheck = frontRepository.findByUserAndPost(userDetails.getUser(), post).orElse(null);
+        if (frontCheck != null) {
+            throw new IllegalArgumentException("이미 프론트엔드에 참여를 하셨습니다.");
+        }
         // 눌렀는지 확인하기
         Back backCheck = backRepository.findByUserAndPost(userDetails.getUser(), post).orElse(null);
         User user = userRepository.findById(userDetails.getUser().getId()).orElseThrow(
@@ -47,9 +47,18 @@ public class BackService {
         // 좋아요를 안눌렀을 경우(null) -> Repository에 포스트와 유저정보를 저장
         // 이후 해당 포스트의 리스트 갯수를 센다음 게시글 Entity에 Cnt에 넣어준다.
         Back back = new Back();
-        if (backCheck == null) {
+        if (backCheck != null) {
+            // 좋아요를 눌렀었을 경우, 요청이 들어오면 Repository에서
+            backRepository.deleteById(backCheck.getId());
+
+            List<Back> backList = backRepository.findAllByPost(post);
+
+            post.setBackCnt(backList.size());
+            post.setCompleted(false);
+            return backCheck.toBackDto(false, false, backList.size());
+        } else {
             // 모집이 완료되면 종료
-            if (post.getBackNum() == post.getBackCnt()){
+            if (post.getBackNum() == post.getBackCnt()) {
                 throw new IllegalArgumentException("정원 초과입니다.");
             }
             back = backRepository.save(backRequestDto.toBackEntity(post, user));
@@ -57,22 +66,12 @@ public class BackService {
 
             post.setBackCnt(backList.size());
             //만약 백엔드 모집인원과 참여인원 수가 같은면 모집완료 처리
-            if (post.getFrontNum() == post.getFrontCnt() && post.getBackNum() == post.getBackCnt()){
+            if (post.getFrontNum() == post.getFrontCnt() && post.getBackNum() == post.getBackCnt()) {
                 post.setCompleted(true);
                 return back.toBackDto(true, true, backList.size());
             }
             //반환값으로 좋아요 누른 bool값과 게시글에대한 총 좋아요 갯수를 보낸다.
-            return back.toBackDto(true,false, backList.size());
-        }
-        else {
-            // 좋아요를 눌렀었을 경우, 요청이 들어오면 Repository에서
-            backRepository.deleteById(backCheck.getId());
-
-            List<Back> backList = backRepository.findAllByPost(post);
-
-            post.setBackCnt(backList.size());
-
-            return backCheck.toBackDto(false,false, backList.size());
+            return back.toBackDto(true, false, backList.size());
         }
     }
 }
--- src/main/java/com/sparta/mini2/service/FrontService.java
+++ src/main/java/com/sparta/mini2/service/FrontService.java
@@ -36,7 +36,7 @@ public class FrontService {
         );
         // 다른 포지션에서 참여를 햇는지 검사
         Back backCheck = backRepository.findByUserAndPost(userDetails.getUser(), post).orElse(null);
-        if (backCheck != null){
+        if (backCheck != null) {
             throw new IllegalArgumentException("이미 백엔드에 참여를 하셨습니다.");
         }
         // 눌렀는지 확인하기
@@ -47,9 +47,19 @@ public class FrontService {
         // 좋아요를 안눌렀을 경우(null) -> Repository에 포스트와 유저정보를 저장
         // 이후 해당 포스트의 리스트 갯수를 센다음 게시글 Entity에 Cnt에 넣어준다.
         Front front = new Front();
-        if (frontCheck == null) {
-//             모집이 완료되면 종료
-            if (post.getFrontNum() == post.getFrontCnt()){
+        if (frontCheck != null) {
+            // 좋아요를 눌렀었을 경우, 요청이 들어오면 Repository에서
+            frontRepository.deleteById(frontCheck.getId());
+
+            List<Front> frontsList = frontRepository.findAllByPost(post);
+
+            post.setFrontCnt(frontsList.size());
+            post.setCompleted(false);
+            return frontCheck.toFrontDto(false, false, frontsList.size());
+
+        } else {
+            // 모집이 완료되면 종료
+            if (post.getFrontNum() == post.getFrontCnt()) {
                 throw new IllegalArgumentException("정원 초과입니다.");
             }
             front = frontRepository.save(frontRequestDto.toFrontEntity(post, user));
@@ -57,22 +67,12 @@ public class FrontService {
 
             post.setFrontCnt(frontList.size());
             //만약 프론트 모집인원과 참여인원 수가 같은면 모집완료 처리
-            if (post.getFrontNum() == post.getFrontCnt() && post.getBackNum() == post.getBackCnt()){
+            if (post.getFrontNum() == post.getFrontCnt() && post.getBackNum() == post.getBackCnt()) {
                 post.setCompleted(true);
-                return front.toFrontDto(true,true, frontList.size());
+                return front.toFrontDto(true, true, frontList.size());
             }
             //반환값으로 좋아요 누른 bool값과 게시글에대한 총 좋아요 갯수를 보낸다.
-            return front.toFrontDto(true,false, frontList.size());
-        }
-        else {
-            // 좋아요를 눌렀었을 경우, 요청이 들어오면 Repository에서
-            frontRepository.deleteById(frontCheck.getId());
-
-            List<Front> frontsList = frontRepository.findAllByPost(post);
-
-            post.setFrontCnt(frontsList.size());
-
-            return frontCheck.toFrontDto(false,false, frontsList.size());
+            return front.toFrontDto(true, false, frontList.size());
         }
     }
 }
