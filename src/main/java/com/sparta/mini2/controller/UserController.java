f5b46eb543ab9f8eb3de42731fe37fc632ca9375
ohyeryung <100130070+ohyeryung@users.noreply.github.com>
2022-04-15 20:33:06 +0900

    찐찐찐막 (+리드미 수정)

--- src/main/java/com/sparta/mini2/controller/UserController.java
+++ src/main/java/com/sparta/mini2/controller/UserController.java
@@ -8,7 +8,6 @@ import com.sparta.mini2.validator.UserInfoValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
